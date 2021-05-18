04fbb851fe0a4d996447670ffef1a99d4b5192b1
Sergio Rodríguez Gómez <46205341+srsergiorodriguez@users.noreply.github.com>
2021-05-18 10:52:45 -0500

    Update sketch.js

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,7 +1,8 @@
 function setup() {
   noCanvas();
   loaderGUI('counter', countRegex);
-  loaderGUI('flourish-formatter', formatFlourish);
+  loaderGUI('count-filter', countFilter);
+  loaderGUI('matrix-formatter', formatMatrix);
   loaderGUI('media-preview', previewImgs);
 }
 
@@ -29,119 +30,49 @@ function loaderGUI(name, func) {
   })
 }
 
-function previewImgs(container, blob) {
+function countFilter(container, blob) {
   d3.csv(blob, d=>d).then(data => {
-    if (selectAll(".preview-div")) {selectAll(".preview-div").map(d=>d.remove())};
-    const previewDiv = createDiv().class("preview-div").parent(container);
+    if (selectAll(".count-filter-div")) {selectAll(".count-filter-div").map(d=>d.remove())};
+    const filterDiv = createDiv().class("count-filter-div").parent(container);
 
-    createP("Selecciona la columna con media").parent(previewDiv)
-    const mediaColumn = createSelect().parent(previewDiv);
+    createP("Selecciona la columna con respecto a la cual se va a filtrar").parent(filterDiv)
+    const filterColumn = createSelect().parent(filterDiv);
     for (let e of data.columns) {
-      mediaColumn.option(e);
+      filterColumn.option(e);
     }
 
-    let selection = [];
-
-    createButton("previsualizar").parent(previewDiv).mouseClicked(() => {
-      const mediaList = data.map(d => d[mediaColumn.value()]);
-
-      const imagesDiv = createDiv().parent(previewDiv);
-      const mediaData = [];
-      for (let i = 0; i < 1000; i++) {
-        const url = mediaList[i];
-        let temp;
-        if (url.includes("media")) {
-          temp = {
-            media: mediaList[i],
-            type: "media"
-          }
-          const img = createImg(mediaList[i],"").style("border", "1px solid rgb(0, 0, 0)").style("width","70px").parent(imagesDiv)
-          img.mouseClicked((e)=>{
-            const state = img.style("border") === "4px solid rgb(255, 0, 0)" ? false : true;
-            if (state === true) {
-              selection[i] = data[i];
-            } else if (state === false) {
-              selection[i] = undefined;
-            }
-            const border = state === false ? "1px solid rgb(0, 0, 0)" : state === true ? "4px solid rgb(255, 0, 0)" : "";
-            console.log(mediaList[i]);
-            img.style("border", border);
-          })
-        } else if (url.includes("ext_tw_video")) {
-          if (url.includes(".mp4")) {
-            temp = {
-            media: mediaList[i],
-            type: "ext_tw_video"
-            }
-          } else {
-            temp = {type: "no media"}
-          }
-        } else if (url.includes("tweet_video")) {
-          temp = {
-            media: mediaList[i],
-            type: "tweet_video"
-          }
-        } else {
-          temp = {type: "no media"}
-        }
-        mediaData.push(temp);
-      }
-    });
-
-    createButton("guardar datos de selección").parent(previewDiv).mouseClicked(() => {
-      const filteredSelection = [[data.columns]];
-      for (let e of selection) {
-        if (e !== undefined) {
-          filteredSelection.push(Object.values(e).map(d=>'"'+d+'"'));
-        }
-      }
-      //saveStrings([selection.reduce((a, c) => a + (c !== undefined ? c : "") + "\n","")], "seleccion.txt");
-      createStringDict(filteredSelection).saveTable('seleccionImgs');
-    });
-  });
-}
-
-function formatFlourish(container, blob) {
-  d3.csv(blob, d=>d).then(data => {
-    if (selectAll(".flourish-formatter-div")) {selectAll(".flourish-formatter-div").map(d=>d.remove())};
-    const flourishFormatterDiv = createDiv().class("flourish-formatter-div").parent(container);
-
-    createP("Selecciona el eje X o columnas (p.e. fechas)").parent(flourishFormatterDiv)
-    const groupColumn = createSelect().parent(flourishFormatterDiv);
-    for (let e of data.columns) {
-      groupColumn.option(e);
-    }
-
-    createP("Selecciona los valores numéricos (p.e. frecuencias)").parent(flourishFormatterDiv)
-    const valColumn = createSelect().parent(flourishFormatterDiv);
+    createP("Selecciona la columna con valores").parent(filterDiv)
+    const valColumn = createSelect().parent(filterDiv);
     for (let e of data.columns) {
       valColumn.option(e);
     }
-    valColumn.selected(data.columns[1]);
 
-    createP("Selecciona el eje Y o las filas (p.e. hashtags of palabras)").parent(flourishFormatterDiv)
-    const catColumn = createSelect().parent(flourishFormatterDiv);
-    for (let e of data.columns) {
-      catColumn.option(e);
-    }
-    catColumn.selected(data.columns[2]);
+    createP("¿Cuantos quieres obtener luego del filtrado?").parent(filterDiv);
+    const valNum = createInput(2).parent(filterDiv);
 
-    createButton("formatear").parent(flourishFormatterDiv).mouseClicked(() => {
-      const categories = [...new Set(data.map(d => d[catColumn.value()]))];
-      const catIndex = {};
-      for (let i = 0; i < categories.length; i++) {
-        catIndex[categories[i]] = i;
-      }
-      const formatted = {grupo: categories};
-      const roll = d3.rollups(data, v => v[0][valColumn.value()], d => d[groupColumn.value()], d => d[catColumn.value()]);
+    const sortSelect = createSelect().parent(filterDiv);
+    sortSelect.option("mayores");
+    sortSelect.option("menores");
+    sortSelect.selected("mayores");
 
-      for (group of roll) {
-        formatted[group[0]] = new Array(categories.length).fill("0");
-        for (category of group[1]) {
-          formatted[group[0]][catIndex[category[0]]] = category[1];
+    createButton("contar").parent(filterDiv).mouseClicked(() => {
+      const generalCounts = {};
+      for (let e of data) {
+        if (generalCounts[e[filterColumn.value()]] === undefined) {
+          generalCounts[e[filterColumn.value()]] = +e[valColumn.value()];
+        } else {
+          generalCounts[e[filterColumn.value()]] += +e[valColumn.value()];
         }
       }
-      createStringDict(formatted).saveTable('formateadoFlourish');
+
+      const sortFunction = sortSelect.value() === "mayores" ? d3.descending : d3.ascending;
+      const generalEntries = Object.entries(generalCounts).sort((a,b) => sortFunction(a[1], b[1])).slice(0, +valNum.value());
+      console.log(generalEntries);
+      const validList = generalEntries.map(d => d[0]);
+      const filtered = data.filter(d => validList.includes(d[filterColumn.value()])).map(d => Object.values(d));
+      const newData = [[...data.columns], ...filtered];
+
+      createStringDict(newData).saveTable('filtrado');   
     });
   });
 }
@@ -223,13 +154,126 @@ function countRegex(container, blob) {
             counts.push([group, exp, precount[group][exp]]);
           }
         }
-        console.log(counts);
-        createStringDict(counts).saveTable('tabla'); 
-        
-        // console.log(counts);
-        // createStringDict(counts).saveTable('tabla');        
+        createStringDict(counts).saveTable('tabla');     
       }
       
     })
+  });
+}
+
+function formatMatrix(container, blob) {
+  d3.csv(blob, d=>d).then(data => {
+    if (selectAll(".matrix-formatter-div")) {selectAll(".matrix-formatter-div").map(d=>d.remove())};
+    const matrixFormatterDiv = createDiv().class("matrix-formatter-div").parent(container);
+
+    createP("Selecciona el eje X o columnas (p.e. fechas)").parent(matrixFormatterDiv)
+    const groupColumn = createSelect().parent(matrixFormatterDiv);
+    for (let e of data.columns) {
+      groupColumn.option(e);
+    }
+
+    createP("Selecciona los valores numéricos (p.e. frecuencias)").parent(matrixFormatterDiv)
+    const valColumn = createSelect().parent(matrixFormatterDiv);
+    for (let e of data.columns) {
+      valColumn.option(e);
+    }
+    valColumn.selected(data.columns[1]);
+
+    createP("Selecciona el eje Y o las filas (p.e. hashtags of palabras)").parent(matrixFormatterDiv)
+    const catColumn = createSelect().parent(matrixFormatterDiv);
+    for (let e of data.columns) {
+      catColumn.option(e);
+    }
+    catColumn.selected(data.columns[2]);
+
+    createButton("formatear").parent(matrixFormatterDiv).mouseClicked(() => {
+      const categories = [...new Set(data.map(d => d[catColumn.value()]))];
+      const catIndex = {};
+      for (let i = 0; i < categories.length; i++) {
+        catIndex[categories[i]] = i;
+      }
+      const formatted = {grupo: categories};
+      const roll = d3.rollups(data, v => v[0][valColumn.value()], d => d[groupColumn.value()], d => d[catColumn.value()]);
+
+      for (group of roll) {
+        formatted[group[0]] = new Array(categories.length).fill("0");
+        for (category of group[1]) {
+          formatted[group[0]][catIndex[category[0]]] = category[1];
+        }
+      }
+      createStringDict(formatted).saveTable('formateadoMatriz');
+    });
+  });
+}
+
+function previewImgs(container, blob) {
+  d3.csv(blob, d=>d).then(data => {
+    if (selectAll(".preview-div")) {selectAll(".preview-div").map(d=>d.remove())};
+    const previewDiv = createDiv().class("preview-div").parent(container);
+
+    createP("Selecciona la columna con media").parent(previewDiv)
+    const mediaColumn = createSelect().parent(previewDiv);
+    for (let e of data.columns) {
+      mediaColumn.option(e);
+    }
+
+    let selection = [];
+
+    createButton("previsualizar").parent(previewDiv).mouseClicked(() => {
+      const mediaList = data.map(d => d[mediaColumn.value()]);
+
+      const imagesDiv = createDiv().class("masonry").parent(previewDiv);
+      const mediaData = [];
+      for (let i = 0; i < 2000; i++) {
+        const url = mediaList[i];
+        let temp;
+        if (url.includes("media")) {
+          temp = {
+            media: mediaList[i],
+            type: "media"
+          }
+          let selected = false;
+          const img = createImg(mediaList[i],"").class("masonry-item").parent(imagesDiv);
+          img.mouseClicked((e) => {
+            selected = selected === true ? false : true;
+            if (selected === true) {
+              selection[i] = data[i];
+            } else if (selected === false) {
+              selection[i] = undefined;
+            }
+            const border = selected === false ? "2px solid rgb(0, 0, 0)" : selected === true ? "4px solid rgb(255, 0, 0)" : "";
+            console.log(mediaList[i]);
+            img.style("border", border);
+          })
+        } else if (url.includes("ext_tw_video")) {
+          if (url.includes(".mp4")) {
+            temp = {
+            media: mediaList[i],
+            type: "ext_tw_video"
+            }
+          } else {
+            temp = {type: "no media"}
+          }
+        } else if (url.includes("tweet_video")) {
+          temp = {
+            media: mediaList[i],
+            type: "tweet_video"
+          }
+        } else {
+          temp = {type: "no media"}
+        }
+        mediaData.push(temp);
+      }
+    });
+
+    createButton("guardar datos de selección").parent(previewDiv).mouseClicked(() => {
+      const filteredSelection = [[data.columns]];
+      for (let e of selection) {
+        if (e !== undefined) {
+          filteredSelection.push(Object.values(e).map(d=>'"'+d+'"'));
+        }
+      }
+      createStringDict(filteredSelection).saveTable('seleccionImgs');
+    });
   });
 }
